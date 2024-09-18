668dbc1e10861611a2088115a3a6a8c38dbc3c25
John Patrick Marcellana <jptmarcellana@gmail.com>
2024-09-18 22:06:35 +0800

    implemented screen -s <name>. pending ui design/format for process screen

--- CSOPESY OS Project/CSOPESY OS Project.cpp
+++ CSOPESY OS Project/CSOPESY OS Project.cpp
@@ -14,7 +14,16 @@ using namespace std;
 
 #include <iostream>
 #include <cctype>
+#include <vector>
+#include <ctime>
 
+// structure to represent a screen process
+struct Screen {
+    string processName;
+    int currentLine;
+    int totalLine;
+    string timestamp;
+};
 
 void header() {
     cout << "             ________________________________________________\n";
@@ -49,11 +58,52 @@ void newLine() {
     cout << "\n";
 }
 
+void clearScreen() {
+    system("cls");
+    header();
+    newLine();
+}
+
+string getCurrentTimestamp() {
+    // Get current time as time_t object
+    std::time_t currentTime = std::time(nullptr);
+
+    // Create tm structure to store local time
+    std::tm localTime;
+
+    // Convert time_t to tm structure (safe version using localtime_s)
+    localtime_s(&localTime, &currentTime);
+
+    // Create a buffer to store the formatted time
+    char timeBuffer[100];
+
+    // Format the time (MM/DD/YYYY, HH:MM:SS AM/PM)
+    std::strftime(timeBuffer, sizeof(timeBuffer), "%m/%d/%Y, %I:%M:%S %p", &localTime);
+
+    return timeBuffer;
+}
+
+void displayScreen(Screen screen) {
+    cout << "Process name: " << screen.processName << endl;
+    cout << "Line: " << screen.currentLine << " of " << screen.totalLine << endl;
+    cout << "Created on: " << screen.timestamp << endl;
+    cout << "Type 'exit' to return to the main menu" << endl;
+    
+    string processCommand;
+    while (processCommand != "exit") {
+        cout << "Enter command: ";
+        cin >> processCommand;
+    }
+    clearScreen();
+}
+
 int main()
 {
     header();
     newLine();
     string command = "";
+
+    vector<Screen> screens;
 
     while (command != "exit") {
         cout << "Enter command: ";
@@ -65,10 +115,6 @@ int main()
 
         if (command == "initialize") {
             cout << "'initialize' command recognized. Doing something.";
-            newLine();
-        }
-        else if (command == "screen") {
-            cout << "'screen' command recognized. Doing something.";
             newLine();
         }
         else if (command == "scheduler-test") {
@@ -84,9 +130,28 @@ int main()
             newLine();
         }       
         else if (command == "clear") {
-            system("cls");
-            header();
-            newLine();
+            clearScreen();
+        }
+        else if (command == "screen") {
+            string screenCommand;
+
+            cin >> screenCommand;
+
+            // creates a new process
+            if (screenCommand == "-s") {
+                // get the process name
+                string processName;
+                cin >> processName;
+
+                if (!processName.empty()) {
+                    // create new instance of the screen ( hard line of instructions )
+                    Screen newScreen = { processName, 1, 10, getCurrentTimestamp() };
+                    // push the new process to the vector
+                    screens.push_back(newScreen);
+                    system("cls");
+                    displayScreen(newScreen);
+                }
+            }
         }
         else if (command != "exit") {
             cout << "'" << command << "' command not recognized.";
