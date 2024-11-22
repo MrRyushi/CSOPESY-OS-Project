0588b3d63c1b6253f636744be49d1237c2ebb7bc
carlosgutierrez121 <134764909+carlosgutierrez121@users.noreply.github.com>
2024-11-22 19:45:57 +0800

    fixed error: removed duplicate attribute

--- CSOPESY OS Project/PagingAllocator.h
+++ CSOPESY OS Project/PagingAllocator.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Screen.h"
 #include <unordered_map>
+#include <map>
 
 class PagingAllocator
 {
@@ -21,8 +22,6 @@ private:
 
 	size_t maxMemorySize;
 	size_t numFrames;
-	std::unordered_map<size_t, string> frameMap;
-	std::vector<size_t, size_t> freeFrameList;
 
 	size_t allocateFrames(size_t numFrames, string processName);
 	void deallocateFrames(size_t numFrames, size_t frameIndex);
